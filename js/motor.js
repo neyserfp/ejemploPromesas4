c41e684b8a1f2064adb1ea5d8e13ca229d53d94c
neyserfp <neyser@digitechfp.com>
2024-02-14 13:52:57 +0100

    Cambios 2

--- js/motor.js
+++ js/motor.js
@@ -1,13 +1,11 @@
-const data1 = {nombre: "Velasquez", localidad: "Sevilla"}
+const data1 = { nombre: "Velazquez", localidad: "Sevilla" };
 
 // Llamada a la promesa (sintaxis 2)
 async function postJSON(data1, url1, control1)
 {
     try
     {
-
         // Envío de datos al Servidor
-
         const response = await fetch(url1, {
             method: "POST",
             headers: {
@@ -19,13 +17,16 @@ async function postJSON(data1, url1, control1)
         // Devolución de datos del Servidor
         const result = await response.json();
         // Opción 1: Recepción de los datos provenientes de una Array Indexada
-        control1.innerHTML = result[0] + "<br>" + result[1];
+        //control1.innerHTML = result[0] + "<br>" + result[1];
         // Opción 2: Recepción de los datos provenientes de una Array Asociativa
+        control1.innerHTML = result.nom01 + "<br>" + result.loc01;
+
+        console.log("Success:", result);
 
     }
     catch(error)
     {
-        console.log(error.message);
+        console.log(error);
     }
 }
 
